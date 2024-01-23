1a1fb790d398cde8cf8d59187f9a01bd84b16c2d
lenamin <lenaminn@gmail.com>
2024-01-23 15:24:28 +0900

    [FEAT] 기존 작업 추가

---
+++ Wetox-iOS/Wetox-iOS/View/FriendsCollectionViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  File.swift
+//  Wetox-iOS
+//
+//  Created by Lena on 1/23/24.
+//
+
+import UIKit
+
+class FriendsCollectionViewCell: UICollectionViewCell {
+    
+    static let cellIdentifier = "FriendsCollectionViewCell"
+    
+    var circularProfileProgressBar: CircularProgressBar = {
+        let progressBar = CircularProgressBar()
+        progressBar.backgroundColor = .systemBackground
+        return progressBar
+    }()
+    
+    var nicknameLabel: UILabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        circularProfileProgressBar.setProgress(self.bounds)
+        setupLayout()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        circularProfileProgressBar.setProgress(bounds)
+    }
+
+    
+    /// 재사용 시 value 값을 초기화
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        circularProfileProgressBar.value = nil
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        self.backgroundColor = .systemBackground
+        
+        nicknameLabel.setLabel(labelText: "lena",
+                               backgroundColor: .clear,
+                               weight: .medium,
+                               textSize: 14,
+                               labelColor: UIColor.unselectedTintColor)
+        
+        [circularProfileProgressBar, nicknameLabel].forEach { self.addSubview($0)}
+        circularProfileProgressBar.profileImageView.image = UIImage(systemName: "person.fill")
+        circularProfileProgressBar.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+        nicknameLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(self)
+            make.top.equalTo(circularProfileProgressBar.snp.bottom)
+        }
+    }
+    
+}
---
+++ Wetox-iOS/Wetox-iOS/View/MainViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  Wetox-iOS
+//
+//  Created by Lena on 1/23/24.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    
+    private var segmentedControl: UISegmentedControl = {
+        let items = ["week", "daily", "badge"]
+        let segmentedControl = UISegmentedControl(items: items)
+        segmentedControl.addTarget(self, action: #selector(tappedSegmentedControl(_:)), for: .valueChanged)
+        segmentedControl.selectedSegmentIndex = 1
+        
+        segmentedControl.frame = CGRectMake(0, 0, 180, 36)
+        segmentedControl.layer.cornerRadius = 5.0
+        segmentedControl.backgroundColor = .segmentedBackgroundGrayColor
+        segmentedControl.tintColor = .gray
+        return segmentedControl
+    }()
+    
+    private var friendsCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 16
+        layout.scrollDirection = .vertical
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        [segmentedControl, friendsCollectionView].forEach { view.addSubview($0) }
+        configureLayout()
+        configureUnselectedSegmentedControl()
+        setupCollectionView()
+    }
+    
+    private func configureLayout() {
+        segmentedControl.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.leading.equalTo(view).offset(16)
+            make.width.equalTo(180)
+        }
+        
+        friendsCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(segmentedControl.snp_bottomMargin).offset(16)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
+    }
+    
+    
+    @objc func tappedSegmentedControl(_ segmentedControl: UISegmentedControl) {
+        configureUnselectedSegmentedControl()
+        switch (segmentedControl.selectedSegmentIndex) {
+            case 0:
+                segmentedControl.setImage(UIImage(named: "weekly-selected-icon"), forSegmentAt: 0)
+            case 1:
+                segmentedControl.setImage(UIImage(named: "daily-selected-icon"), forSegmentAt: 1)
+            case 2:
+                segmentedControl.setImage(UIImage(named: "badge-selected-icon"), forSegmentAt: 2)
+            default:
+                break
+        }
+    }
+    
+    func configureUnselectedSegmentedControl() {
+        segmentedControl.setImage(UIImage(named: "weekly-unselected-icon"), forSegmentAt: 0)
+        segmentedControl.setImage(UIImage(named: "daily-unselected-icon"), forSegmentAt: 1)
+        segmentedControl.setImage(UIImage(named: "badge-unselected-icon"), forSegmentAt: 2)
+    }
+    
+    func setupCollectionView() {
+        friendsCollectionView.register(FriendsCollectionViewCell.self, forCellWithReuseIdentifier: FriendsCollectionViewCell.cellIdentifier)
+        friendsCollectionView.dataSource = self
+        friendsCollectionView.delegate = self
+    }
+}
+
+extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 14
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FriendsCollectionViewCell.cellIdentifier, for: indexPath) as! FriendsCollectionViewCell
+        // 셀의 내용 설정 또는 데이터 로딩
+        cell.circularProfileProgressBar.value = 0.64
+    
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = (collectionView.frame.width - 32) / 3
+        print("collectionViewCell의 CGSize: \(cellWidth)")
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+}
+                                  
