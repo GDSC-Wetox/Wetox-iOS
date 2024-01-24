be57d43deef03f928c365a03a451f9976d19c454
SohyeonKim-dev <happythgus89@gmail.com>
2024-01-24 10:07:14 +0900

    [FEAT] Button Extension 추가

--- Wetox-iOS/Wetox-iOS/Extension/Button+Extension.swift
+++ Wetox-iOS/Wetox-iOS/Extension/Button+Extension.swift
@@ -5,4 +5,19 @@
 //  Created by 김소현 on 1/24/24.
 //
 
-import Foundation
+import UIKit.UIButton
+
+extension UIButton {
+    
+    func createButton(title: String, titleSize: Int, titleColor: UIColor, backgroundColor: UIColor) {
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = .systemFont(ofSize: CGFloat(titleSize), weight: .bold)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+    }
+    
+    func makeRounded(radius: CGFloat) {
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
+}
--- Wetox-iOS/Wetox-iOS/View/LoginViewController.swift
+++ Wetox-iOS/Wetox-iOS/View/LoginViewController.swift
@@ -5,4 +5,49 @@
 //  Created by 김소현 on 1/24/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class LoginViewController: UIViewController {
+    
+    var subTitleLabel: UILabel = UILabel()
+    var titleLabel: UILabel = UILabel()
+
+    private func setupLayout() {
+        
+        subTitleLabel.setLabel(labelText: "우리의 Detox Mate",
+                            backgroundColor: .clear,
+                            weight: .light,
+                            textSize: 18,
+                            labelColor: .black)
+        
+        titleLabel.setLabel(labelText: "Wetox",
+                            backgroundColor: .clear,
+                            weight: .medium,
+                            textSize: 35,
+                            labelColor: .black)
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        [subTitleLabel, titleLabel].forEach { view.addSubview($0) }
+
+        setupLayout()
+        configureLayout()
+    }
+    
+    private func configureLayout() {
+        subTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(138)
+            make.leading.trailing.equalToSuperview().inset(24)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(162)
+            make.leading.trailing.equalToSuperview().inset(24)
+        }
+    }
+}
