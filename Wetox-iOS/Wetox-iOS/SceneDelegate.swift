7a9061ca4c5eab4ef6b0e4da84d1c9b30f37d62b
SohyeonKim-dev <happythgus89@gmail.com>
2024-01-26 16:14:47 +0900

    [FEAT] SceneDelegate KakaoSDKAuth 추가

--- Wetox-iOS/Wetox-iOS/SceneDelegate.swift
+++ Wetox-iOS/Wetox-iOS/SceneDelegate.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    // MARK: KakaoSDK
+    // ref: https://seungchan.tistory.com/entry/Swift-카카오-소셜-로그인
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
