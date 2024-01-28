db834b566d45cdf4b40160001bfe0933358b1706
lenamin <lenaminn@gmail.com>
2024-01-28 19:36:51 +0900

    [ADD] 네비게이션 삭제 및 CustomUISlider 구조 변경으로 인한 프로퍼티 수정

--- Wetox-iOS/Wetox-iOS/View/ScreenTimeInputView/ScreenTimeInputViewController.swift
+++ Wetox-iOS/Wetox-iOS/View/ScreenTimeInputView/ScreenTimeInputViewController.swift
@@ -12,24 +12,24 @@ import RxSwift
 class ScreenTimeInputViewController: UIViewController {
     
     /*
-     - 네비게이션 바 타이틀, done 버튼
      - 스크린타임 확인하는 안내문 <-> 오류 생기는 경우 레이블
      - CustomUISlider 만들어서 재사용할 수 있도록
      - 입력받는 모델 구현
      */
     
     var sliders: [CustomUISlider] = []
+    let categories = Constants.Category.categories
     private let infoTitleLabel = UILabel()
     private let infoContentLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        setNavigation()
         [infoTitleLabel, infoContentLabel].forEach { view.addSubview($0) }
         
-        for _ in 0..<Constants.Category.categories.count {
+        for i in 0..<categories.count {
             let slider = CustomUISlider()
+            slider.categoryLabel.text = categories[i]
             view.addSubview(slider)
             sliders.append(slider)
         }
@@ -37,16 +37,12 @@ class ScreenTimeInputViewController: UIViewController {
         customizeLabel()
         configureLayout()
     }
-    
-    private func setNavigation() {
-        self.title = Constants.ScreenTimeInput.navigationTitle
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: Constants.Common.doneText, style: .done, target: self, action: #selector(doneAction))
-        
-    }
-    
+
     private func customizeLabel() {
         infoTitleLabel.setLabel(labelText: Constants.ScreenTimeInput.titleMessage, backgroundColor: .clear, weight: .semibold, textSize: Constants.font.semiTitleSize, labelColor: .lightGray)
+        
         infoContentLabel.setLabel(labelText: Constants.ScreenTimeInput.infoMessage, backgroundColor: .clear, weight: .medium, textSize: Constants.font.contentSize, labelColor: .darkGray)
+        
         infoContentLabel.numberOfLines = 0
     }
     
@@ -56,6 +52,7 @@ class ScreenTimeInputViewController: UIViewController {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(Constants.Padding.defaultPadding)
             $0.leading.trailing.equalTo(view).inset(Constants.Padding.defaultPadding)
         }
+        
         infoContentLabel.snp.makeConstraints {
             $0.top.equalTo(infoTitleLabel.snp.bottom)
             $0.leading.trailing.equalTo(view).inset(Constants.Padding.defaultPadding)
@@ -70,12 +67,5 @@ class ScreenTimeInputViewController: UIViewController {
             }
             previousSlider = slider
         }
-        
-        
-    }
-    
-    @objc func doneAction() {
-        // 완료 버튼을 눌렀을 때의 액션을 여기에 구현하세요.
-        print("ScreenTimeInputView에서 done 버튼을 눌렀습니다. ")
     }
 }
