4296e5f684850996c78d1522e6aed2fa52ddac01
SohyeonKim-dev <happythgus89@gmail.com>
2024-01-28 11:57:38 +0900

    [FEAT] Network Service Auth API

--- Wetox-iOS/Wetox-iOS/NetworkService/Auth/AuthAPI.swift
+++ Wetox-iOS/Wetox-iOS/NetworkService/Auth/AuthAPI.swift
@@ -5,4 +5,144 @@
 //  Created by 김소현 on 1/28/24.
 //
 
-import Foundation
+import UIKit
+import Moya
+
+public class AuthAPI {
+    static let shared = AuthAPI()
+    var authProvider = MoyaProvider<AuthService>(plugins: [MoyaLoggerPlugin()])
+    
+    public init() { }
+    
+    // MARK: - 소셜 회원가입
+    func socialSignUp(socialSignUpRequest: SocialSignUpRequest, profileImage: UIImage?, completion: @escaping (NetworkResult<Any>) -> Void) {
+        authProvider.request(.socialSignUp(SocialSignUpRequest: SocialSignUpRequest, profileImage: profileImage)) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeSocialSignUpStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let error):
+                print("error: \(error)")
+            }
+        }
+    }
+    
+    // MARK: - 로그인
+    func login(loginRequest: LoginRequest, completion: @escaping (NetworkResult<Any>) -> Void) {
+        authProvider.request(.login(loginRequest: loginRequest)) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeLoginStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let error):
+                print("error: \(error)")
+            }
+        }
+    }
+    
+    // MARK: - 로그아웃
+    func logout(completion: @escaping (NetworkResult<Any>) -> Void) {
+        authProvider.request(.logout) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeLoginStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let error):
+                print("error: \(error)")
+            }
+        }
+    }
+    
+    // MARK: - 회원 탈퇴
+    func withdrawal(completion: @escaping (NetworkResult<Any>) -> Void) {
+        authProvider.request(.withdrawal) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeWithdrawalStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let error):
+                print("error: \(error)")
+            }
+        }
+    }
+    
+    private func judgeSocialSignUpStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<SocialSignUpResponse>.self, from: data) else { return .pathError }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "None-data")
+        case 400..<500:
+            return .requestError(decodedData.resultCode, decodedData.message)
+        case 500:
+            return .serverError
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func judgeLoginStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<LoginResponse>.self, from: data) else { return .pathError }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "None-data")
+        case 400..<500:
+            return .requestError(decodedData.resultCode, decodedData.message)
+        case 500:
+            return .serverError
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func judgeLogoutStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<String>.self, from: data) else { return .pathError }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData.data ?? "None-data")
+        case 400..<500:
+            return .requestError(decodedData.resultCode, decodedData.message)
+        case 500:
+            return .serverError
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func judgeWithdrawalStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<String>.self, from: data) else { return .pathError }
+        
+        switch statusCode {
+        case 200:
+            return .success(decodedData)
+        case 400..<500:
+            return .requestError(decodedData.resultCode, decodedData.message)
+        case 500:
+            return .serverError
+        default:
+            return .networkFail
+        }
+    }
+}
--- Wetox-iOS/Wetox-iOS/NetworkService/Auth/AuthService.swift
+++ Wetox-iOS/Wetox-iOS/NetworkService/Auth/AuthService.swift
@@ -5,4 +5,88 @@
 //  Created by 김소현 on 1/28/24.
 //
 
-import Foundation
+import UIKit
+import Moya
+
+enum AuthService {
+    case socialSignUp(SocialSignUpRequest: SocialSignUpRequest, profileImage: UIImage?)
+    case login(loginRequest: LoginRequest) // 로그인
+    case logout // 로그아웃
+    case withdrawal // 회원 탈퇴
+}
+
+extension AuthService: TargetType {
+    // MARK: - baseURL (서버의 endpoint 도메인)
+    var baseURL: URL {
+        return URL(string: "baseURL")!
+    }
+    
+    // MARK: - path (도메인 뒤에 추가될 path)
+    var path: String {
+        switch self {
+        case .socialSignUp:
+            return "/auth/social-signup"
+        case .login:
+            return "/auth/login"
+        case .logout:
+            return "/auth/logout"
+        case .withdrawal:
+            return "/auth/withdrawal"
+        }
+    }
+    
+    // MARK: - HTTP method (get, post, patch, delete)
+    var method: Moya.Method {
+        switch self {
+        case .socialSignUp, .login, .logout:
+            return .post
+        case .withdrawal:
+            return .delete
+        }
+    }
+    
+    // MARK: - task (request에 사용될 파라미터)
+    // 서버에 보낼 데이터가 없다면 .requestPlain
+    // 파라미터로 보낼 데이터가 있다면 .requestParameters (ex. http://dev.umc-bonheur.shop/api/boards?memberId=1&page=0&size=1&sort=string)
+    // 서버에 보낼 데이터(body)가 있다면 .requestJSONEncodable
+    // 참고: https://moya.github.io/Enums/Task.html
+    
+    var task: Moya.Task {
+        switch self {
+        case .socialSignUp(let socialSignUpJSON, let profileImage):
+            var multipartFormData: [MultipartFormData] = []
+            
+            let requestData = try? JSONEncoder().encode(socialSignUpJSON)
+            multipartFormData.append(MultipartFormData(provider: .data(requestData!), name: "socialSignUpRequest", mimeType: "application/json"))
+            if profileImage != nil {
+                let imageData = profileImage!.jpegData(compressionQuality: 1.0)
+                multipartFormData.append(MultipartFormData(provider: .data(imageData!), name: "profileImage", fileName: "image.jpg", mimeType: "image/gif"))
+            }
+            
+            return .uploadMultipart(multipartFormData)
+            
+        case .login(let loginRequest):
+            return .requestJSONEncodable(loginRequest)
+            
+        case .withdrawal:
+            return .requestPlain
+
+        case .logout:
+            return .requestPlain
+        }
+    }
+    
+    // MARK: - HTTP Header
+//    var headers: [String: String]? {
+//        switch self {
+//        case .socialSignUp:
+//            return Const.Header.multipartHeader
+//        case .login:
+//            return .none
+//        case .logout, .withdrawal:
+//            let sessionId = UserDefaults.standard.string(forKey: Const.UserDefaultsKey.sessionId)
+//            let authorizationHeader = ["Content-Type": "application/json", "Authorization": sessionId ?? ""]
+//            return authorizationHeader
+//        }
+//    }
+}
