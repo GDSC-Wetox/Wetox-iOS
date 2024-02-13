308512ad24788dd7d1789edd3cecc6867519e134
lenamin <lenaminn@gmail.com>
2024-02-13 12:48:20 +0900

    [FEAT] Main - UserAPI 바인딩 뷰모델 구현

--- Wetox-iOS/Wetox-iOS/ViewModel/MainViewModel.swift
+++ Wetox-iOS/Wetox-iOS/ViewModel/MainViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class MainViewModel {
+    
+    let disposeBag = DisposeBag()
+    
+    let myProfile: Observable<(nickname: String, imageName: String)>
+    
+    init(userAPI: UserAPI = .init()) {
+        myProfile = UserAPI.getUserInfo()
+            .map { ($0.nickname, $0.profileImage) }
+            .catch { _ in Observable.just(("unknown", "person")) }
+            .share(replay: 1, scope: .whileConnected)
+    }
+}
