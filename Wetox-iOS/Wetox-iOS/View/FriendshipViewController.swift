4c60e07ec016a8ba4a937f6542b7c200c11bb836
SohyeonKim-dev <happythgus89@gmail.com>
2024-02-15 18:08:57 +0900

    [ADD] FriendshipViewController

---
+++ Wetox-iOS/Wetox-iOS/View/FriendshipViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FriendshipViewController.swift
+//  Wetox-iOS
+//
+//  Created by 김소현 on 2/15/24.
+//
+
+import UIKit
+import SnapKit
+
+class FriendshipViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
