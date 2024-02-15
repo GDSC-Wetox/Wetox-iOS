845673ecfdd4bc6e0bc56cff0a7c0f60d5f23010
SohyeonKim-dev <happythgus89@gmail.com>
2024-02-15 18:09:30 +0900

    [FEAT] Friendship network model 구현

---
+++ Wetox-iOS/Wetox-iOS/NetworkModel/Friendship/getFriendsListResponse.swift
@@ -0,0 +1,18 @@
+//
+//  getFriendsListResponse.swift
+//  Wetox-iOS
+//
+//  Created by 김소현 on 2/15/24.
+//
+
+import Foundation
+
+struct getFriendsListResponse: Codable {
+    let FriendsList: [Friend]
+}
+
+struct Friend: Codable {
+    let userId: Int64
+    let nickname: String
+    let totalDuration: Int64
+}
