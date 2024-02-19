4f089176763d50285ed6064fd0731befa43df355
SohyeonKim-dev <happythgus89@gmail.com>
2024-02-19 17:45:28 +0900

    [FEAT] AlertTableViewCell Added

--- Wetox-iOS/Wetox-iOS/View/AlertTableViewCell.swift
+++ Wetox-iOS/Wetox-iOS/View/AlertTableViewCell.swift
@@ -5,4 +5,51 @@
 //  Created by 김소현 on 2/18/24.
 //
 
-import Foundation
+import UIKit
+
+protocol AlertTableViewCellDelegate: AnyObject {
+    func acceptButtonTapped(at indexPath: IndexPath)
+    func rejectButtonTapped(at indexPath: IndexPath)
+}
+
+class AlertTableViewCell: UITableViewCell {
+    static let reuseIdentifier = "AlertTableCell"
+    
+    let nameLabel = UILabel()
+    let acceptButton = UIButton(type: .system)
+    let rejectButton = UIButton(type: .system)
+    
+    weak var delegate: AlertTableViewCellDelegate?
+    var indexPath: IndexPath?
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(acceptButton)
+        contentView.addSubview(rejectButton)
+        
+        acceptButton.setTitle("수락", for: .normal)
+        acceptButton.addTarget(self, action: #selector(acceptButtonTapped), for: .touchUpInside)
+        
+        rejectButton.setTitle("거절", for: .normal)
+        rejectButton.addTarget(self, action: #selector(rejectButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func acceptButtonTapped() {
+        guard let indexPath = indexPath else { return }
+        delegate?.acceptButtonTapped(at: indexPath)
+    }
+    
+    @objc private func rejectButtonTapped() {
+        guard let indexPath = indexPath else { return }
+        delegate?.rejectButtonTapped(at: indexPath)
+    }
+}
--- Wetox-iOS/Wetox-iOS/View/AlertViewController.swift
+++ Wetox-iOS/Wetox-iOS/View/AlertViewController.swift
@@ -11,10 +11,69 @@ import RxSwift
 
 class AlertViewController: UIViewController {
     let disposeBag = DisposeBag()
+    let tableView = UITableView()
     
+    private var navigationButton: UIButton = UIButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(AlertTableViewCell.self, forCellReuseIdentifier: AlertTableViewCell.reuseIdentifier)
+
+        
+        self.navigationItem.title = "알림"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: navigationButton)
+        
+        [navigationButton, tableView].forEach { view.addSubview($0) }
+        
+        configureLayout()
     }
     
+    private func configureLayout() {
+        navigationButton.setTitle("닫기", for: .normal)
+        navigationButton.setTitleColor(.systemBlue, for: .normal)
+        navigationButton.addTarget(self, action: #selector(navigationButtonTapped), for: .touchUpInside)
+        
+        tableView.snp.makeConstraints { make in
+             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+             make.leading.trailing.bottom.equalToSuperview()
+         }
+    }
+    
+    @objc func navigationButtonTapped() {
+        self.navigationController?.popViewController(animated: false)
+    }
+}
+
+extension AlertViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 // 예시로 10개의 셀을 표시
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: AlertTableViewCell.reuseIdentifier, for: indexPath) as! AlertTableViewCell
+        
+        // 셀에 데이터 설정
+        cell.nameLabel.text = "Friend \(indexPath.row + 1)"
+        
+        cell.delegate = self
+        cell.indexPath = indexPath
+        
+        return cell
+    }
+}
+
+extension AlertViewController: AlertTableViewCellDelegate {
+    func acceptButtonTapped(at indexPath: IndexPath) {
+        // 해당 indexPath에 대한 수락 처리
+        print("Accept button tapped at indexPath: \(indexPath)")
+    }
+    
+    func rejectButtonTapped(at indexPath: IndexPath) {
+        // 해당 indexPath에 대한 거절 처리
+        print("Reject button tapped at indexPath: \(indexPath)")
+    }
 }
