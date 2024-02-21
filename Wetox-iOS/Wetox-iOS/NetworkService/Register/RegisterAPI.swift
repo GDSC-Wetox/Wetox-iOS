3b30da2af2e45903241a0b07a2b7d35e107ba607
SohyeonKim-dev <happythgus89@gmail.com>
2024-02-21 19:27:06 +0900

    [FEAT] Foldering 및 Register 네트워크 모델 추가

---
+++ Wetox-iOS/Wetox-iOS/NetworkService/Register/RegisterAPI.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterAPI.swift
+//  Wetox-iOS
+//
+//  Created by 김소현 on 2/21/24.
+//
+
+import Foundation
---
+++ Wetox-iOS/Wetox-iOS/NetworkService/Register/RegisterService.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterService.swift
+//  Wetox-iOS
+//
+//  Created by 김소현 on 2/21/24.
+//
+
+import Foundation
